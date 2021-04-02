c2ce6e02f995e4c5e71f64a957b7bc21373bd5ce
hieuluu-lulu <hieulvtgcd17213@fpt.edu.vn>
2021-04-02 16:03:17 +0700

    fix guest view

--- resources/views/guest/guestDashboard.blade.php
+++ resources/views/guest/guestDashboard.blade.php
@@ -63,7 +63,7 @@
   </aside>
 @section('guest_content')
 
-@if($getGuestEmail) 
+@if($getGuestEmail)
   <div class="content-header">      
     <div class="container-fluid">
       <div class="row mb-2">
@@ -72,48 +72,45 @@
         </div><!-- /.col -->
       </div>
     </div>
+      
+      
     @foreach($getData as $key => $getDatas)
     <div class="card-body pb-0 main1">  
       <div class="row d-flex align-items-stretch main2">
-        <div class="col-12 col-sm-6 col-md-6 d-flex align-items-stretch">
-          <div class="card bg-light">
-            <div class="card-header text-muted border-bottom-0">
+          <div class="col-12 col-sm-6 col-md-6 d-flex align-items-stretch">
+            <div class="card bg-light">
+              <div class="card-header text-muted border-bottom-0">
                 File Upload By Student
-            </div>
-            <div class="card-body pt-0">
-              <div class="row">
-                <div class="col-7">
-                  <input type="hidden" id="getId" value="{{$getDatas->student_id}}">
-                  <input type="hidden" id="getURL" value="{{url('executeComment/'.$getDatas->student_id)}}">
-                  <h2 class="lead"><b id="file1">{{$getDatas->student_uploadfile}}</b></h2>
-                  <p class="text-muted text-sm"><b>Description: </b>{{$getDatas->student_description}}</p>
-                  <p class="text-muted text-sm"><b>Date Upload: </b>{{$getDatas->created_at}}</p>
-                  <p class="text-muted text-sm"><b>Faculty: </b>{{$getDatas->faculity_name}}</p>
-                </div>
-                <div class="col-5 text-center">
-                  <img src="{{asset("imageStudent/$getDatas->student_uploadimage")}}" class="img-circle img-fluid" alt="">
-                </div>
               </div>
+              <div class="card-body pt-0">
+                <div class="row">
+                  <div class="col-7">
+                    <input type="hidden" id="getId" value="{{$getDatas->student_id}}">
+                    <h2 class="lead"><b id="file1">{{$getDatas->student_uploadfile}}</b></h2>
+                    <p class="text-muted text-sm"><b>Description: </b>{{$getDatas->student_description}}</p>
+                    <p class="text-muted text-sm"><b>Date Upload: </b>{{$getDatas->created_at}}</p>
+                    <p class="text-muted text-sm"><b>Faculty: </b>{{$getDatas->faculity_name}}</p>
+                  </div>
+                  <div class="col-5 text-center">
+                    <img src="{{asset("imageStudent/$getDatas->student_uploadimage")}}" class="img-circle img-fluid" alt="">
+                  </div>
+                </div>
               </div>
               <div class="card-footer">
-                <div class="text-right"></div>
+                <div class="text-right">
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    @endforeach
+      @endforeach
   </div>
   @else
-    <div class="content-header">
-      <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1 class="m-0 text-dark">Please <a href="{{route('ACCOUNT')}}">login</a> first!</h1>
-            </div><!-- /.col -->
-          </div>
-        </div>
-    </div>
+  <div class="container-fluid content-header">
+    <h1 class="m-0 text-dark">Please <a href= "{{route('ACCOUNT')}}">login</a> first!</h1>
+  </div>
 @endif
+
+
 @endsection
