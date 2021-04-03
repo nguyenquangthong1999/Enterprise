a5aebbc1a1b724db82e8a1265140e412218968f9
luxurydemon123 <51477712+luxurydemon123@users.noreply.github.com>
2021-04-03 12:45:53 +0700

    hdfsdfsdF

--- resources/views/admin/semester/edit_semester.blade.php
+++ resources/views/admin/semester/edit_semester.blade.php
@@ -22,15 +22,9 @@
     <div class="container-fluid">
 
       <div class="row">
-<<<<<<< HEAD
  
         <div class="col-md-6">
          
-=======
-
-        <div class="col-md-6">
-
->>>>>>> fd72615... fsadfsdfsdf
             <div class="card-body">
                 <!-- Date -->
             <form action="{{url('semester/'. $semester_edit->semester_id)}}" method="POST">
@@ -40,10 +34,7 @@
                     <label for="exampleInputEmail1">Name Semester</label>
                     <input type="text" name="semester" required class="form-control" value="{{$semester_edit->semester_name}}" id="exampleInputEmail1" placeholder="Input name semester">
                 </div>
-<<<<<<< HEAD
                 
-=======
->>>>>>> fd72615... fsadfsdfsdf
                 <div class="form-group">
                     <label>Start Date:</label>
                     <input type="date" name="startdate" value="{{$semester_edit->start_date}}" required id="datepicker" class="form-control">
@@ -53,7 +44,6 @@
                     <label>End Date:</label>
                     <input type="date" name="enddate" value="{{$semester_edit->end_date}}" required id="datepicker2" class="form-control">
                 </div>
-<<<<<<< HEAD
             
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
@@ -61,15 +51,6 @@
           
         
           
-=======
-
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-          </div>
-
-
-
->>>>>>> fd72615... fsadfsdfsdf
         </div>
         <!-- /.col -->
       </div>
@@ -77,8 +58,4 @@
     </div>
     <!-- /.container-fluid -->
   </section>
-<<<<<<< HEAD
 @endsection
-=======
-@endsection 
->>>>>>> fd72615... fsadfsdfsdf
--- routes/web.php
+++ routes/web.php
@@ -79,10 +79,6 @@ Route::post('semester', 'SemesterController@addSemesterProcess');
 Route::delete('semester/{id}','SemesterController@deleteSemester');
 Route::get('semester/edit/{id}','SemesterController@editSemester');
 Route::patch('semester/{id}' , 'SemesterController@editSemesterProcess');
-<<<<<<< HEAD
-
-=======
->>>>>>> fd72615... fsadfsdfsdf
 //  Marketing manager
 Route::get('manager','ManagerController@ManagerDashboard') -> name('MANAGER');
 Route::get('statistics','ManagerController@ViewStatistics')->name('STATISTICS');
